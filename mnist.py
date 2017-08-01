bd9dbb5fcdfc7b2912dc72441feb5150851cf1fe
mledall <matthias.ledall08@gmail.com>
2017-08-01 09:24:52 -0700

    created train data loading function

--- mnist.py
+++ mnist.py
@@ -8,10 +8,18 @@ import tensorflow as tf
 # we are going to use pandas, http://pandas.pydata.org/pandas-docs/stable/
 import pandas as pd
 
-txt = pd.read_csv('train.csv', sep = ',', header = 0)
 
-#print txt
+def load_train_data():
+	file_path = 'train.csv'
+	txt = pd.read_csv(file_path, sep = ',', header = 0)
+	X = txt.values.copy()
+	np.random.shuffle(X)	# randomize the input arrays
+	labels = X[:,0]
+	train = X[:,1:]		# Use a StandardScaler() on the pixel data? Is there any advantage?
+	print 'training data input size: ', np.shape(train)
+	print 'labels input size: ', np.shape(labels)
+	return train, labels
 
-v = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+load_train_data()
 
-print txt['0']
+
